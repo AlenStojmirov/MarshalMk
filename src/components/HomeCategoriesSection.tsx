a773976f4c3ca6b438bfc2732e7c331bef22d90b
Alen <stojmirovalen@gmail.com>
2026-05-05 12:35:32 +0200

    fixes

--- src/components/HomeCategoriesSection.tsx
+++ src/components/HomeCategoriesSection.tsx
@@ -1,25 +1,30 @@
+'use client';
+
 import CategoryCard from './CategoryCard';
 import MobileCarousel from './MobileCarousel';
-
-const categories = [
-  {
-    title: 'Поло Маици',
-    href: '/mens/polos',
-    imageSrc: 'https://i.postimg.cc/MpmMDHmB/well2.webp',
-  },
-  {
-    title: 'Фармерки',
-    href: '/mens/jeans',
-    imageSrc: 'https://i.postimg.cc/qRPBXxPw/Fotografija-11.jpg',
-  },
-  {
-    title: 'Елеци',
-    href: '/mens/vests',
-    imageSrc: 'https://i.postimg.cc/jjqWkTM7/well7.webp',
-  }
-];
+import { useTranslation } from '@/lib/i18n/useTranslation';
 
 export default function HomeCategoriesSection() {
+  const { t } = useTranslation();
+
+  const categories = [
+    {
+      title: t('home.categoryPolos'),
+      href: '/mens/polos',
+      imageSrc: 'https://i.postimg.cc/MpmMDHmB/well2.webp',
+    },
+    {
+      title: t('home.categoryJeans'),
+      href: '/mens/jeans',
+      imageSrc: 'https://i.postimg.cc/qRPBXxPw/Fotografija-11.jpg',
+    },
+    {
+      title: t('home.categoryVests'),
+      href: '/mens/vests',
+      imageSrc: 'https://i.postimg.cc/jjqWkTM7/well7.webp',
+    },
+  ];
+
   return (
     <section>
       {/* Desktop: 3-column grid */}
